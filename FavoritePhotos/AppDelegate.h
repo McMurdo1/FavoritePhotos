829387a874440b08a7531e40942cbe05b729c07c
McMurdo1 <matt@1423ventures.com>
2014-01-27 15:03:52 -0600

    Initial Commit

---
+++ FavoritePhotos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavoritePhotos
+//
+//  Created by Matthew Graham on 1/27/14.
+//  Copyright (c) 2014 Matthew Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FavoritePhotos/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FavoritePhotos
+//
+//  Created by Matthew Graham on 1/27/14.
+//  Copyright (c) 2014 Matthew Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
